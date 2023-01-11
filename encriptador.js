6e45f805b98124f3d3ad868d818d98e6ae372609
Pablo Rojas <pablorojas654@gmail.com>
2023-01-11 20:22:51 -0300

    primer commit

---
+++ encriptador.js
@@ -0,0 +1,5 @@
+function encriptar(){
+    var texto = document.getElementById("input-encriptador");
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("resultado").innerHTML=texto;
+}
