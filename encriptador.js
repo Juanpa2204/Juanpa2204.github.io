b92b6b119ac54920f24cf2c6c356cfa9e92ee099
pablo rojas <pablorojas654@gmail.com>
2023-01-29 20:48:18 -0300

    logica encriptador

--- encriptador.js
+++ encriptador.js
@@ -1,5 +1,23 @@
-function encriptar(){
-    var texto = document.getElementById("input-encriptador");
-    document.getElementById("texto").style.display = "none";
-    document.getElementById("resultado").innerHTML=texto;
+const textArea = document.querySelector(".input-encriptador");
+const mensaje = document.querySelector(".resultado");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value=""; 
+    mensaje.style.backgroundImage= "none";
 }
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["a","ai"],["e","enter"], ["i","imes"],["o","ober"],["u","ufat"]];
+    stringEncriptado=stringEncriptado.toLowerCase();
+   
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado=stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i],[1]);
+        }        
+    }
+    return stringEncriptado; 
+}
+
+
