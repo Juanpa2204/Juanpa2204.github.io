b2ffc21f93a098613b97a8e973e7b05e3eb455cd
pablo rojas <pablorojas654@gmail.com>
2023-01-31 02:33:51 -0300

    act 31/01 efectos botones, parte logica

--- encriptador.js
+++ encriptador.js
@@ -1,23 +1,51 @@
 const textArea = document.querySelector(".input-encriptador");
 const mensaje = document.querySelector(".resultado");
 
-function btnEncriptar(){
+function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value);
     mensaje.value = textoEncriptado;
-    textArea.value=""; 
-    mensaje.style.backgroundImage= "none";
+    textArea.value = "";
+    document.querySelector(".avisos").style.display='none';
+    document.querySelector(".resultado").style.display='block';
+    document.querySelector('.boton-copiar').style.display ='block';
+   
 }
 
-function encriptar(stringEncriptado){
-    let matrizCodigo = [["a","ai"],["e","enter"], ["i","imes"],["o","ober"],["u","ufat"]];
-    stringEncriptado=stringEncriptado.toLowerCase();
-   
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptado.includes(matrizCodigo[i][0])) {
-            stringEncriptado=stringEncriptado.replaceAll(matrizCodigo[i][0],matrizCodigo[i],[1]);
-        }        
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
     }
-    return stringEncriptado; 
+    return stringEncriptado;
 }
 
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+
+
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
+
+function updateClipboard() {
+    var contenido = document.querySelector(".resultado");
+    contenido.select();
+    navigator.clipboard.writeText(contenido.value);
+}
 
